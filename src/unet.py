5161193fed2b437548261389a004b977657f2897
taashi <x7286x_ayk@yahoo.co.jp>
2018-04-03 20:51:46 +0900

    add BatchNormalization, Dropout

--- src/unet.py
+++ src/unet.py
@@ -1,9 +1,10 @@
 from keras.models import Model
 from keras.engine.topology import Input
-from keras.layers.core import Flatten, Dense, Reshape
+from keras.layers.core import Flatten, Dense, Reshape, Dropout, Activation
 from keras.layers.convolutional import Conv2D, UpSampling2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.merge import concatenate
+from keras.layers.normalization import BatchNormalization
 
 
 class UNet(object):
@@ -22,9 +23,9 @@ class UNet(object):
         decodeLayer1 = self.__add_decode_layers(
             512, encodeLayer5, encodeLayer4)
         decodeLayer2 = self.__add_decode_layers(
-            256, decodeLayer1, encodeLayer3)
+            256, decodeLayer1, encodeLayer3, add_drop_layer=True)
         decodeLayer3 = self.__add_decode_layers(
-            128, decodeLayer2, encodeLayer2)
+            128, decodeLayer2, encodeLayer2, add_drop_layer=True)
         decodeLayer4 = self.__add_decode_layers(
             64, decodeLayer3, encodeLayer1)
 
@@ -36,24 +37,26 @@ class UNet(object):
     def __add_encode_layers(self, filter_size, input_layer, is_first=False):
         layer = input_layer
         if is_first:
-            layer = Conv2D(filter_size, 3, padding='same', activation='relu', input_shape=(
+            layer = Conv2D(filter_size, 3, padding='same', input_shape=(
                 self.INPUT_SIZE, self.INPUT_SIZE, 1))(layer)
         else:
             layer = MaxPooling2D(2)(layer)
-            layer = Conv2D(filter_size, 3, padding='same',
-                           activation='relu')(layer)
-        layer = Conv2D(filter_size, 3, padding='same',
-                       activation='relu')(layer)
+            layer = Conv2D(filter_size, 3, padding='same')(layer)
+        layer = Conv2D(filter_size, 3, padding='same')(layer)
+        layer = BatchNormalization()(layer)
+        layer = Activation(activation='relu')(layer)
         print(layer.shape)
         return layer
 
-    def __add_decode_layers(self, filter_size, input_layer, concat_layer):
+    def __add_decode_layers(self, filter_size, input_layer, concat_layer, add_drop_layer=False):
         layer = UpSampling2D(2)(input_layer)
         layer = concatenate([layer, concat_layer])
-        layer = Conv2D(filter_size, 3, padding='same',
-                       activation='relu')(layer)
-        layer = Conv2D(filter_size, 3, padding='same',
-                       activation='relu')(layer)
+        layer = Conv2D(filter_size, 3, padding='same')(layer)
+        layer = Conv2D(filter_size, 3, padding='same')(layer)
+        layer = BatchNormalization()(layer)
+        layer = Activation(activation='relu')(layer)
+        if add_drop_layer:
+            layer = Dropout(0.5)(layer)
         print(layer.shape)
         return layer
 
